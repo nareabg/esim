ca660e80d32ed83583611d6025905121098ca56f
nare abgaryan <nareabgaryan2001@gmail.com>
2023-04-20 19:40:45 +0400

    aaaaaa

--- zenq/api/prepare_db.py
+++ zenq/api/prepare_db.py
@@ -1,25 +1,64 @@
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
-from sqlalchemy.orm import declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy_utils import database_exists, create_database
+# from sqlalchemy.orm import declarative_base
 
 # import sys
 # sys.path.append("..")
-from .tables import metadata
+# from tables import metadata
 
+
+# for t in metadata.sorted_tables:
+#     print(t.name)
+    
+# print("-"*50)
+
+# # Base = declarative_base()
+# # metadata = Base.metadata
+
+# username = "aua"
+# password = "mysecretpassword"
+# hostname = "localhost"  # This is the database's service name which is 'database'.
+# port = "5432"  # Note that this is the port inside the 'database' container.
+# db_name = "GLOBBING"
+# DB_URL = f"postgresql://{username}:{password}@{hostname}:{port}/{db_name}"
+
+# class db():
+#     def main(self):
+#         print("Initializing the database..", end=" ")
+    
+#         engine = create_engine(DB_URL)
+#         if not database_exists(engine.url):
+#             create_database(engine.url)
+        
+#         # Create the defined tables.
+#         metadata.create_all(bind=engine)
+    
+#         print("done")
+    
+    
+# if __name__ == "_main_":
+#     main()
+    
+#  # go to extension press plus and enter info   
+
+
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import declarative_base
+import sys
+sys.path.append("..")
+from tables import metadata
 
 for t in metadata.sorted_tables:
     print(t.name)
     
 print("-"*50)
 
-# Base = declarative_base()
-# metadata = Base.metadata
-
-username = "postgres"
+username = "aua"
 password = "mysecretpassword"
-hostname = "localhost"  # This is the database's service name which is 'database'.
-port = "5432"  # Note that this is the port inside the 'database' container.
-db_name = "globbing"
+hostname = "localhost"  
+port = "5432"  
+db_name = "GLOBBING"
 DB_URL = f"postgresql://{username}:{password}@{hostname}:{port}/{db_name}"
 
 class db():
@@ -35,8 +74,6 @@ class db():
     
         print("done")
     
-    
-if __name__ == "_main_":
-    main()
-    
- # go to extension press plus and enter info   
+if __name__ == "__main__":
+    mydb = db()
+    mydb.main()
--- zenq/api/tables.py
+++ zenq/api/tables.py
@@ -10,7 +10,7 @@ from sqlalchemy import text
 
 
 
-engine = create_engine('postgresql://postgres:mysecretpassword@localhost:5432/globbing')
+engine = create_engine('postgresql://aua:mysecretpassword@localhost:5432/GLOBBING')
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 metadata = Base.metadata
@@ -47,74 +47,74 @@ with engine.connect() as conn:
 
 
 class Customer(Base):
-    __tablename__ = 'Customer'
-    __table_args__ = {'schema': 'initial'}
+        __tablename__ = 'Customer'
+        __table_args__ = {'schema': 'initial'}
 
-    id = Column(Integer, primary_key=True)
-    customer_id = Column(String(50), unique=True, nullable=False)
-    gender = Column(String(10))
+        id = Column(Integer, primary_key=True)
+        customer_id = Column(String(50), unique=True, nullable=False)
+        gender = Column(String(10))
 
-    def __repr__(self):
-        return f"<Customer(id={self.id}, customer_id='{self.customer_id}', gender='{self.gender}')>"
+        def __repr__(self):
+            return f"<Customer(id={self.id}, customer_id='{self.customer_id}', gender='{self.gender}')>"
 
 
 class Facts(Base):
-    __tablename__ = 'Facts'
-    __table_args__ = {'schema': 'initial'}
+        __tablename__ = 'Facts'
+        __table_args__ = {'schema': 'initial'}
 
-    id = Column(Integer, primary_key=True)
-    customer_id_uniq = Column(String(50), ForeignKey('initial.Customer.id'), nullable=False)
-    location_id = Column(String(50), nullable=False)
-    location_name = Column(String(50), nullable=False)
-    invoice_id = Column(String(50),unique= True, nullable=False)
-    date = Column(DateTime, nullable=False)
-    quantity = Column(Float, nullable=False)
-    total_price = Column(Float, nullable=False)
-    gender = Column(String(10))
-    
-    @property
-    def unit_price(self):
-        return self.total_price / self.quantity
+        id = Column(Integer, primary_key=True)
+        customer_id_uniq = Column(Integer, ForeignKey('initial.Customer.id'), nullable=False)
+        location_id = Column(String(50), nullable=False)
+        location_name = Column(String(50), nullable=False)
+        invoice_id = Column(String(50),unique= True, nullable=False)
+        date = Column(DateTime, nullable=False)
+        quantity = Column(Float, nullable=False)
+        total_price = Column(Float, nullable=False)
+        gender = Column(String(10))
+        
+        @property
+        def unit_price(self):
+            return self.total_price / self.quantity
 
-    # def __repr__(self):
-    #     return f"<Facts(id={self.id}, customer_id='{self.customer_id}', location_id='{self.location_id}', date='{self.date}', quantity={self.quantity}, total_price={self.total_price}, gender='{self.gender}', unit_price={self.unit_price})>"
+        # def __repr__(self):
+        #     return f"<Facts(id={self.id}, customer_id='{self.customer_id}', location_id='{self.location_id}', date='{self.date}', quantity={self.quantity}, total_price={self.total_price}, gender='{self.gender}', unit_price={self.unit_price})>"
 
 
 class CustomerFact(Base):
-    __tablename__ = 'CustomerFact'
-    __table_args__ = {'schema': 'result'}
+        __tablename__ = 'CustomerFact'
+        __table_args__ = {'schema': 'result'}
 
-    id = Column(Integer, primary_key=True)
-    customer_id = Column(String(50), unique=True, nullable=False)
-    date = Column(Integer, nullable=False)
-    invoice_id = Column(Integer, nullable=False)
-    quantity = Column(Float, nullable=False)
-    price = Column(Float, nullable=False)
-    CLV = Column(Float, nullable=False)
-    
+        id = Column(Integer, primary_key=True)
+        customer_id = Column(String(50), unique=True, nullable=False)
+        date = Column(Integer, nullable=False)
+        invoice_id = Column(Integer, nullable=False)
+        quantity = Column(Float, nullable=False)
+        price = Column(Float, nullable=False)
+        CLV = Column(Float, nullable=False)
+        
 
-    # def _repr_(self):
-    #     return f"<CustomerFact(id={self.id}, customer_id='{self.customer_id}', date='{self.date}', invocie_id='{self.invoice_id}', quantity='{self.quantity}', price='{self.price}', CLV='{self.CLV}')>"
+        # def _repr_(self):
+        #     return f"<CustomerFact(id={self.id}, customer_id='{self.customer_id}', date='{self.date}', invocie_id='{self.invoice_id}', quantity='{self.quantity}', price='{self.price}', CLV='{self.CLV}')>"
 
 
 
 class Prediction(Base):
-    __tablename__  = 'Prediction'
-    __table_args__ = {'schema': 'result'}
+        __tablename__  = 'Prediction'
+        __table_args__ = {'schema': 'result'}
 
-    id = Column(Integer, primary_key=True)
-    customer_id = Column(String(50), unique=True, nullable=False)
-    recency = Column(Float, nullable=False)
-    frequency = Column(Integer, nullable=False)
-    monetary = Column(Float, nullable=False)
-    T = Column(Float, nullable=False)
-    pred_1month = Column(Float, nullable=False)
+        id = Column(Integer, primary_key=True)
+        customer_id = Column(String(50), unique=True, nullable=False)
+        recency = Column(Float, nullable=False)
+        frequency = Column(Integer, nullable=False)
+        monetary = Column(Float, nullable=False)
+        T = Column(Float, nullable=False)
+        pred_1month = Column(Float, nullable=False)
 
 
-    # def _repr_(self):
-    #     return f"<CustomerFact(id={self.id}, customer_id='{self.customer_id}', recency='{self.recency}', frequency='{self.frequency}', monetary='{self.monetary}', T='{self.T}', pred_1month='{self.pred_1month}')>"
+        # def _repr_(self):
+        #     return f"<CustomerFact(id={self.id}, customer_id='{self.customer_id}', recency='{self.recency}', frequency='{self.frequency}', monetary='{self.monetary}', T='{self.T}', pred_1month='{self.pred_1month}')>"
 
 for t in metadata.sorted_tables:
     print(t.name)
-    
+        
 print("-"*50)
