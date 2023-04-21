bc585e531de0a595aea527fc3ed9e34cdb8bb6c6
nare abgaryan <nareabgaryan2001@gmail.com>
2023-04-21 21:10:31 +0400

    bvc

--- jamanakavor.py
+++ jamanakavor.py
@@ -262,3 +262,61 @@ class points():
         self.session.commit()
  
  # create, deletem,  read
+ 
+ 
+     def insert_facts(self):
+        # Get the list of available columns from the DataFrame
+        available_columns = self.df.columns.tolist()
+
+        # Create dropdown widgets for the user to choose columns
+        column_dropdowns = [widgets.Dropdown(options=available_columns, description=f'Map to {col}')
+                            for col in ['location_id', 'location_name', 'invoice_id', 'date', 'quantity', 'total_price']]
+
+        # Display the dropdown widgets
+        display(*column_dropdowns)
+
+        # Wait for the user to select columns and click the 'Submit' button
+        submit_button = widgets.Button(description='Submit')
+        display(submit_button)
+        output = widgets.Output()
+
+        def on_submit_button_clicked(b):
+            with output:
+                # Get the chosen column names
+                column_names = [dropdown.value for dropdown in column_dropdowns]
+
+                # Insert the data into the Facts table
+                for idx, (index, row) in enumerate(self.df.iterrows()):
+                    # get the customer_id from the input data
+                    # customer_id =  row[column_names[0]]
+                    customer_id =  self.session.query(Customer).all()
+                    # print(customer_id[idx].id)
+                    # print(self.session.query(Customer))
+                    # query the database for the corresponding Customer instance
+                    # customer = self.session.query(Customer).filter_by(customer_id=customer_id[idx].id).all()
+                    # print(customer)
+                    # create a new Facts instance and set its customer attribute
+                    facts = Facts(
+                        customer_id_uniq=customer_id[idx].id,
+                        location_id=row[column_names[0]],
+                        location_name=row[column_names[1]],
+                        invoice_id=row[column_names[2]],
+                        date=row[column_names[3]],
+                        quantity=row[column_names[4]],
+                        total_price=row[column_names[5]]
+                    )
+
+                    try:
+                        # add the new Facts instance to the session and commit the changes
+                        self.session.add(facts)
+                        self.session.commit()
+                        print(f'Successfully inserted facts for customer {customer_id}')
+                    except IntegrityError as e:
+                        # print the error message if an IntegrityError occurs
+                        print(f'Error inserting facts: {str(e)}')
+                        print(f'Facts {row[column_names[0]]}, {row[column_names[1]]}, {row[column_names[2]]}, {row[column_names[3]]}, {row[column_names[4]]}, {row[column_names[5]]} already exists in the table')
+
+            self.session.close()
+
+        submit_button.on_click(on_submit_button_clicked)
+        display(output)
