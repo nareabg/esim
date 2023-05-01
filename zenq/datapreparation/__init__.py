b0bbafcdd0ece42678558aa853cc3e93ed233c04
nare abgaryan <nareabgaryan2001@gmail.com>
2023-05-01 22:09:39 +0400

    prep

--- zenq/datapreparation/__init__.py
+++ zenq/datapreparation/__init__.py
@@ -1,2 +1,3 @@
 #add the imports and the cltv_df
 from zenq.datapreparation.preparation import data_prep
+from zenq.logger import CustomFormatter, bcolors
--- zenq/datapreparation/preparation.py
+++ zenq/datapreparation/preparation.py
@@ -1,6 +1,18 @@
 import os
 import numpy as np
 import pandas as pd
+import logging
+import os
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(funcName)s %(msg)s')
+logger = logging.getLogger(os.path.basename(__file__))
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(CustomFormatter())
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(CustomFormatter())
+logger.addHandler(file_handler)
+logger.addHandler(ch)
 
 class data_prep():
     
@@ -29,14 +41,20 @@ class data_prep():
     def num_of_null(self):
         return self.data.isnull().sum()
 
-    def num_of_unique_in_column(self,column):
+def num_of_unique_in_column(self, column):
+    if column not in self.data.columns:
+        print(f"Error: column '{column}' does not exist in the data. Please try again.")
+        return None
+    else:
         return self.data[column].nunique()
-
-    def finalizing_data(self):
+    logger.error(f"{num_of_unique_in_column.__name__}")
+    
+    def final_data(self):
 
         data = self.data.copy() 
         data = data.drop_duplicates()
         data = data.dropna()
+        logger.info(f"{final_data.__name__}")
         return data
 
 
