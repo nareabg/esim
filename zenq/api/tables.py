b5f85eccfe3b9ff26ac8b3d7f287b3a51f3caf1e
nare abgaryan <nareabgaryan2001@gmail.com>
2023-05-04 15:32:28 +0400

    tables comment

--- zenq/api/tables.py
+++ zenq/api/tables.py
@@ -5,8 +5,11 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 from sqlalchemy.schema import CreateSchema
+
+# Importing custom logger from local packages
 from zenq.logger import CustomFormatter, bcolors
 
+# Basic logging configuration and initialization
 logging.basicConfig(level=logging.DEBUG, format = "/%(asctime)s / %(name)s / %(levelname)s / %(message)s /%(filename)s/%(lineno)d/")
 logger = logging.getLogger(os.path.basename(__file__))
 ch = logging.StreamHandler()
@@ -18,28 +21,37 @@ file_handler.setFormatter(CustomFormatter())
 
 logger.addHandler(file_handler)
 logger.addHandler(ch)
+
+# Create a base class for SQLAlchemy's ORM to inherit from
 Base = declarative_base()
 
 class Facts(Base):
     def connect_to_db(self, db_uri):
         logger.info(f"{self.connect_to_db.__name__}/Connecting to the database.") 
+        # Create a database engine
         engine = create_engine(db_uri)
+        # Create a session factory for the engine
         Session = sessionmaker(bind=engine)
+        # Get the metadata object associated with the database tables
         metadata = Base.metadata
         with engine.connect() as conn:
             try:
+                # Create a schema named 'initial' if it doesn't exist yet
                 conn.execute(CreateSchema('initial', if_not_exists=True))
             except exc.SQLAlchemyError:
                 logger.error(f"{self.connect_to_db.__name__}/Failed to create schema 'initial'")
                 pass
         with engine.connect() as conn:
             try: 
+                # Create a schema named 'result' if it doesn't exist yet
                 conn.execute(CreateSchema('result',  if_not_exists=True))
             except exc.SQLAlchemyError:
                 logger.error(f"{self.connect_to_db.__name__}/Failed to create schema 'result'")
                 pass       
+        # Return the metadata and engine objects for further use    
         return metadata, engine
- 
+    
+    # Define the Facts table
     __tablename__ = 'Facts'
     __table_args__ = {'schema': 'initial'}
 
@@ -50,12 +62,13 @@ class Facts(Base):
     date = Column(DateTime, nullable=False)
     quantity = Column(Float, nullable=False)
     total_price = Column(Float, nullable=False)   
-        
+     
+     # Define a property for unit price, which is calculated by dividing total price by quantity
     @property
     def unit_price(self):
         return self.total_price / self.quantity
     
-
+    # Define the LOGS table
     class LOGS(Base):
         __tablename__ = 'LOGS'
         __table_args__ = {'schema': 'initial'}
@@ -66,7 +79,8 @@ class Facts(Base):
         message = Column(String(2000), nullable=False)
         line_number = Column(Integer, nullable=False)
         load_time = Column(DateTime, nullable=False)     
-              
+    
+    # Define the CLTV table          
     class CLTV(Base):
         __tablename__ = 'CLTV'
         __table_args__ = {'schema': 'result'}
@@ -78,6 +92,7 @@ class Facts(Base):
         frequency = Column(Integer, nullable=False)
         monetary = Column(Float, nullable=False)
 
+    # Define the CustomerAlive table
     class CustomerAlive(Base):
         __tablename__ = 'CustomerAlive'
         __table_args__ = {'schema': 'result'}
@@ -85,6 +100,7 @@ class Facts(Base):
         Customer = Column(String(50), nullable=False)
         Probability_of_being_Alive = Column(Float, nullable=False)
 
+    # Define the Prediction table
     class Prediction(Base):
         __tablename__ = 'Prediction'
         __table_args__ = {'schema': 'result'}
@@ -94,7 +110,8 @@ class Facts(Base):
         Expected_Purchases_90 = Column(Float, nullable=False)
         Expected_Purchases_180 = Column(Float, nullable=False)
         Expected_Purchases_360 = Column(Float, nullable=False)
-        
+    
+    # Define the RFMScore table    
     class RFMScore(Base):
         __tablename__ = 'RFMScore'
         __table_args__ = {'schema': 'result'}
@@ -105,7 +122,8 @@ class Facts(Base):
         monetary_score=Column(Integer, nullable=False)
         RFM_SCORE = Column(Integer, nullable=False)
         segment = Column(String(50), nullable=False)
-        
+    
+    # Define the ParetoParameters table    
     class ParetoParameters(Base):     
         __tablename__ = 'ParetoParameters'
         __table_args__ = {'schema': 'result'}        
